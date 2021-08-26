0072f7ee404e204262f84c0cec47043b8ac053c7
haoyong <hy15613141194@163.com>
2021-08-26 20:41:51 +0800

    部署Linux版Nacos，3个节点，并将服务注册进nacos

--- cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @Slf4j
+@RefreshScope  //支持nacos动态刷新
 public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+    @Value("${config.info}")
+    private String configInfo;
 
     @GetMapping("/payment/nacos/{id}")
     public String getPayment(@PathVariable("id") Integer id) {
         return "访问nacos，端口号：" + serverPort + ",id=" + id;
+    }
+
+    @GetMapping("payment/nacos/getInfo")
+    public String getConfigInfo() {
+        return configInfo;
     }
 }
